c6b8bdccb739780145e7439d5a5c32de0e0723e5
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-24 17:34:48 +0530

    multiple conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import "./styles.css";
 import Counter from "./Counter";
 
 export default function App() {
-  let message = "Hello world";
-  // debugger
-  console.log("test", message);
-
-  const [fruit, setFruit] = useState("apple");
-
-  function handleFruitChange() {
-    setFruit("banana"); // component is re-rendered whenever a state variable is changed
-  }
+  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h3>{fruit}</h3>
-      <button onClick={handleFruitChange}> Change Fruit</button>
-      <Counter />
+      <h2>Toggle in ReactJS</h2>
+      <h1>Count = {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <button onClick={() => setCount(count - 1)}>Decrement Count</button>
+      <button onClick={() => setDisplay(!display)}>Toggle</button>
+      {display ? <Counter /> : null}
+      {count === 0 ? (
+        <h1>Condition 0</h1>
+      ) : count === 1 ? (
+        <h1>Condition 0</h1>
+      ) : count === 2 ? (
+        <h1>Condition 2</h1>
+      ) : count === 3 ? (
+        <h1>Condition 3</h1>
+      ) : (
+        <h1>Other Condition</h1>
+      )}
     </div>
   );
 }
