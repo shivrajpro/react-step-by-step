9d51354ab064f273f7deeb48e80aa1073ebeccc4
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-24 22:41:41 +0530

    lifecycle hooks in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,147 +1,40 @@
-import { useState } from "react";
-import Clock from "./Clock";
+import { useEffect, useState } from "react";
 import "./styles.css";
-import User from "./User";
-import College from "./College";
+import Counter from "./Counter";
 
 export default function App() {
-  const userData = [
-    {
-      name: "Anil",
-      age: 29,
-      email: "anil@test.com",
-      id: 1,
-    },
-    {
-      name: "Sam",
-      age: 36,
-      email: "sam@test.com",
-      id: 2,
-    },
-    {
-      name: "Peter",
-      age: 20,
-      email: "peter@test.com",
-      id: 3,
-    },
-    {
-      name: "Bruce",
-      age: 50,
-      email: "bruce@test.com",
-      id: 4,
-    },
-  ];
-  const collegeData = [
-    {
-      name: "IET Alwer",
-      city: "Alwer",
-      website: "https://www.iet.ac.in/",
-      student: [
-        {
-          name: "Anil sidhu",
-          age: 29,
-          email: "anil@test.com",
-        },
-        {
-          name: "Beter",
-          age: 20,
-          email: "peter@test.com",
-        },
-        {
-          name: "Bruce",
-          age: 25,
-          email: "bruce@test.com",
-        },
-      ],
-    },
-    {
-      name: "IIT Delhi",
-      city: "Delhi",
-      website: "https://www.iit.ac.in/",
-      student: [
-        {
-          name: "Anil sidhu",
-          age: 29,
-          email: "anil@test.com",
-        },
-        {
-          name: "Beter",
-          age: 20,
-          email: "peter@test.com",
-        },
-        {
-          name: "Bruce",
-          age: 25,
-          email: "bruce@test.com",
-        },
-      ],
-    },
-    {
-      name: "KCIET Hisar",
-      city: "Hisar",
-      website: "https://www.kciet.ac.in/",
-      student: [
-        {
-          name: "Anil sidhu",
-          age: 29,
-          email: "anil@test.com",
-        },
-        {
-          name: "Beter",
-          age: 20,
-          email: "peter@test.com",
-        },
-        {
-          name: "Bruce",
-          age: 25,
-          email: "bruce@test.com",
-        },
-      ],
-    },
-  ];
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(4);
 
-  const [color, setColor] = useState("green");
+  function counterFunction() {
+    console.log("counterFunction", count);
+  }
+
+  function dataFunction() {
+    console.log("dataFunction", data);
+  }
+
+  useEffect(() => {
+    counterFunction();
+  }, [count]);
+
+  useEffect(() => {
+    dataFunction();
+  }, [count, data]);
+
+  function callOnce() {
+    console.log("callOnce called");
+  }
+
+  // callOnce();
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
+      <button onClick={() => setData(data + 1)}>Data {data}</button>
 
-      <h2>College Details</h2>
-      {collegeData.map((clg, index) => {
-        return <College key={index} college={clg} />;
-      })}
-      <select value={color} onChange={(evt) => setColor(evt.target.value)}>
-        <option value="red">Red</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-      </select>
-
-      <Clock color={color} />
-      {/* <FormControls /> */}
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {userData.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      {userData.map((user) => (
-        <User key={user.id} user={user} />
-      ))}
+      {count < 5 ? <Counter counter={count} data={data} /> : null}
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
-const Counter = () => {
+const Counter = ({ counter, data }) => {
   const [count, setCount] = useState(0);
 
+  const getCounter = () => {
+    console.log("call only");
+  };
+
+  const handleData = () => {
+    console.log("handleData called");
+  };
+
+  useEffect(() => {
+    handleData();
+  }, [data]);
+
+  getCounter();
+
+  useEffect(() => {
+    return () => {
+      console.log("======ON DESTROY=====");
+    };
+  }, []);
   return (
     <div>
-      <h1>Counter: {count}</h1>
+      <h1>Componen Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Counter : {counter} </h1>
+      <h1>Data : {data} </h1>
     </div>
   );
 };
