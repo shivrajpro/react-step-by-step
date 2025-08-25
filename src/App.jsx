476edf70bc27d82ed90166065ccf9bf4cc15d0ee
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-25 22:16:14 +0530

    external css

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,18 @@
-import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  const [cardStyle, setCardStyle] = useState({
-    border: "1px solid #ccc",
-    width: "200px",
-    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-    margin: "10px",
-  });
-
-  const [grid, setGrid] = useState(true);
-
-  const updateTheme = (bgColor, textColor) => {
-    console.log(bgColor, textColor);
-    setCardStyle({ ...cardStyle, backgroundColor: bgColor, color: textColor });
-  };
-
   return (
     <div>
-      <button onClick={() => updateTheme("gray", "white")}>Gray Theme</button>
-      <button onClick={() => updateTheme("white", "black")}>
-        Default Theme
-      </button>
-      <button onClick={() => setGrid(!grid)}>Toggle Grid</button>
-      <div style={{ display: grid ? "flex" : "block", flexWrap: "wrap" }}>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
+      <h1 className="heading">External CSS</h1>
+      <div className="user-card">
+        <img
+          style={{ width: "200px" }}
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="user-img"
+        />
+        <div style={{ padding: "10px" }}>
+          <h4>John Doe</h4>
+          <p>Software Developer</p>
         </div>
       </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <h1 className="heading">Index.jsx CSS</h1>
     <App />
   </StrictMode>
 );
