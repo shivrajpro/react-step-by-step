a7af130fb55a6ccef8f4427ae0e4a87e48e26e89
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-25 22:03:11 +0530

    dynamic and conditional inline style

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,28 @@
-import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  const cardStyle = {
+  const [cardStyle, setCardStyle] = useState({
     border: "1px solid #ccc",
     width: "200px",
     boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
     margin: "10px",
+  });
+
+  const [grid, setGrid] = useState(true);
+
+  const updateTheme = (bgColor, textColor) => {
+    console.log(bgColor, textColor);
+    setCardStyle({ ...cardStyle, backgroundColor: bgColor, color: textColor });
   };
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="user-img"
-          />
-          <div style={{ padding: "10px" }}>
-            <h4>John Doe</h4>
-            <p>Software Developer</p>
-          </div>
-        </div>
+    <div>
+      <button onClick={() => updateTheme("gray", "white")}>Gray Theme</button>
+      <button onClick={() => updateTheme("white", "black")}>
+        Default Theme
+      </button>
+      <button onClick={() => setGrid(!grid)}>Toggle Grid</button>
+      <div style={{ display: grid ? "flex" : "block", flexWrap: "wrap" }}>
         <div style={cardStyle}>
           <img
             style={{ width: "200px" }}
