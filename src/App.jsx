f5cd0fcc48a35933ec16a9a28ba41b22d401ffd2
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-25 12:30:45 -0500

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import Userprofile from "./Userprofile";
+import styled from "styled-components";
 
-export default function App() {
+// const Heading =styled.h1`
+// color:red;
+// border:1px solid green;
+// border-radius:5px;
+// margin:20px;
+// padding:20px
+// `
+
+const Heading = styled.h1({
+  color: "red",
+  border: "1px solid green",
+  borderRadius: "5px",
+  margin: "20px",
+  padding: "20px",
+});
+
+const StyleBtn = styled.button`
+  color: red;
+  width: 130px;
+  height: 40px;
+  margin: 20px;
+  background-color: ${(props) => props.color || "lightgray"};
+`;
+function App() {
   return (
-    <div>
-      <h1>Style with css module in ReactJS</h1>
-      <Userprofile />
-    </div>
+    <>
+      <h1>Styled Component with React js</h1>
+      <Heading>Hello Heading</Heading>
+      <Heading>Hello Heading 2</Heading>
+      <Heading>Hello Heading 3</Heading>
+      <Heading>Hello Heading 4</Heading>
+      <StyleBtn color="black">Login</StyleBtn>
+      <StyleBtn>Signup</StyleBtn>
+      <StyleBtn>Back</StyleBtn>
+      <StyleBtn>Login</StyleBtn>
+    </>
   );
 }
+export default App;
