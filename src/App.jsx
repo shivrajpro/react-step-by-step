4a4ffbe9fde2816beb6350b7ef82ab992b84f294
Shivraj Hulbatte <41613892+shivrajpro@users.noreply.github.com>
2025-08-26 14:06:20 +0000

    useFormStatus hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useRef } from "react";
-import User from "./User";
-import UserInput from "./UserInput";
+import { useFormStatus } from "react-dom";
 
 function App() {
-  const inputRef = useRef(null);
-
-  const handleInput = () => {
-    inputRef.current.focus();
-    inputRef.current.value = 1000;
+  const handleSubmit = async () => {
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("submit");
   };
 
+  function CustomerForm() {
+    const { pending } = useFormStatus();
+
+    console.log(pending);
+
+    return (
+      <div>
+        <input type="text" placeholder="enter username" />
+        <input type="password" placeholder="enter password" />
+        <button type="submit" disabled={pending}>
+          {pending ? "Submitting" : "Submit"}
+        </button>
+      </div>
+    );
+  }
   return (
     <>
-      <h1>Froward ref</h1>
-      <UserInput ref={inputRef} />
-      <button onClick={handleInput}>Update Input</button>
+      <h1>useFormStatus Hook</h1>
+      <form action={handleSubmit}>
+        <CustomerForm />
+      </form>
     </>
   );
 }
