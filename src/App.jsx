77f28e9d83215ce8159e5981d908e5df8cf08b0a
Shivraj Hulbatte <41613892+shivrajpro@users.noreply.github.com>
2025-08-26 14:24:26 +0000

    useTransition hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useFormStatus } from "react-dom";
+import { useState, useTransition } from "react";
 
 function App() {
+  const [myPending, setMyPending] = useState(false);
+  const [pending, startTransition] = useTransition(false);
+
   const handleSubmit = async () => {
+    setMyPending(true);
+
     await new Promise((res) => setTimeout(res, 2000));
-    console.log("submit");
+
+    setMyPending(false);
   };
 
-  function CustomerForm() {
-    const { pending } = useFormStatus();
-
-    console.log(pending);
-
-    return (
-      <div>
-        <input type="text" placeholder="enter username" />
-        <input type="password" placeholder="enter password" />
-        <button type="submit" disabled={pending}>
-          {pending ? "Submitting" : "Submit"}
-        </button>
-      </div>
-    );
-  }
+  const handleTransition = () => {
+    startTransition(async () => {
+      await new Promise((res) => setTimeout(res, 2000));
+    });
+  };
   return (
     <>
-      <h1>useFormStatus Hook</h1>
-      <form action={handleSubmit}>
-        <CustomerForm />
-      </form>
+      <h1>useTransition Hook</h1>
+      <button disabled={myPending} onClick={handleSubmit}>
+        Click (state)
+      </button>
+      <button disabled={pending} onClick={handleTransition}>
+        Click (transition)
+      </button>
     </>
   );
 }
