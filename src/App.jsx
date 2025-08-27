b0eda66cef0177395019dfd5af88ffb778afbb88
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-27 13:33:42 -0500

    useId hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,41 @@
-import { useActionState } from "react";
+import { useId } from "react";
 
 function App() {
-  const handleSubmit = async (previousData, formData) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log("previousData", previousData);
-    const username = formData.get("username");
-    const password = formData.get("password");
-    console.log("username", username);
-    console.log("password", password);
-
-    if (username && password) {
-      return { message: "Data submitted", username, password };
-    } else {
-      return {
-        error: "Please provide both username and password",
-        username,
-        password,
-      };
-    }
-  };
-
-  const [data, action, pending] = useActionState(handleSubmit, {
-    username: "",
-    password: "",
-  });
-
   return (
     <>
-      <h1>useActionState hook in RectJS</h1>
-      <form action={action}>
-        <input
-          defaultValue={data?.username}
-          type="text"
-          name="username"
-          placeholder="Enter username"
-        />
-        <input
-          defaultValue={data?.password}
-          type="password"
-          name="password"
-          placeholder="Enter password"
-        />
-        <button type="submit" disabled={pending}>
-          {pending ? "Submitting..." : "Submit"}
-        </button>
-      </form>
-
-      {data?.error && <h3 style={{ color: "red" }}>{data.error}</h3>}
-      {data?.message && <h3 style={{ color: "green" }}>{data.message}</h3>}
+      <h1>useId hook in RectJS</h1>
+      <UserForm />
       <hr />
-
-      <h2>Username: {data?.username}</h2>
-      <h2>Password: {data?.password}</h2>
+      <UserForm />
     </>
   );
 }
 export default App;
+
+function UserForm() {
+  const username = useId();
+  const password = useId();
+  const skills = useId();
+  const terms = useId();
+
+  return (
+    <form>
+      <label htmlFor={username}>Enter Username</label>
+      <input id={username} type="text" placeholder="enter username" />
+      <br />
+      <br />
+      <label htmlFor={password}>Enter Password</label>
+      <input id={password} type="password" placeholder="enter password" />
+      <br />
+      <br />
+      <label htmlFor={skills}>Enter Skills</label>
+      <input id={skills} type="text" placeholder="enter username" />
+      <br />
+      <br />
+      <input id={terms} type="checkbox" placeholder="enter username" />
+      <label htmlFor={terms}>Terms and Conditions</label>
+      <br />
+      <br />
+    </form>
+  );
+}
