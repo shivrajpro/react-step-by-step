e0fa0e8f2b718fb982b41d2909f106bbc933f668
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-08-30 00:02:29 +0530

    creating a todo list app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,61 @@
-import { Route, Routes } from "react-router";
-import About from "./About";
-import College from "./College";
-import Department from "./Department";
-import Details from "./Details";
-import Home from "./Home";
-import Login from "./Login";
-import Navbar from "./Navbar";
-import PageNotFound from "./PageNotFound";
-import Student from "./Student";
-import UserDetails from "./UserDetails";
-import Users from "./Users";
+import { useState } from "react";
 
 function App() {
+  const [todoItem, setTodoItem] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = () => {
+    setTodoList([...todoList, { text: todoItem, completed: false }]);
+    setTodoItem("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodoList = todoList.filter((item, i) => i !== index);
+    setTodoList(newTodoList);
+  };
+
+  const completeTodo = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList[index].completed = !newTodoList[index].completed;
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
       <h1>App Component</h1>
+      <input
+        type="text"
+        value={todoItem}
+        onChange={(e) => setTodoItem(e.target.value)}
+        placeholder="Enter Todo Item"
+      />
+      <button onClick={() => addTodo()}>Add</button>
+      <ul style={{ listStyleType: "none" }}>
+        {todoList.map((item, index) => {
+          return (
+            <li key={index}>
+              <h3>
+                <input
+                  type="checkbox"
+                  id={"item" + index}
+                  checked={item?.completed}
+                  onChange={() => completeTodo(index)}
+                />
+                <label
+                  htmlFor={"item" + index}
+                  style={{
+                    textDecoration: item?.completed ? "line-through" : "none",
+                  }}
+                >
+                  {item?.text}
+                </label>{" "}
+                &nbsp;
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+              </h3>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
