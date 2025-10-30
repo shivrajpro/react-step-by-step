1fd66d44bef8c17629fce71d9ac992be4c4a7f41
Shivraj Hulbatte <41613892+shivrajpro@users.noreply.github.com>
2025-10-30 05:43:11 +0000

    crud with todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import { lazy, Suspense, useState } from "react";
-// import ClassComponent from "./ClassComponent";
+import TodoList from "./components/todo-list";
 
-const ClassComponent = lazy(() => import("./ClassComponent"));
 function App() {
-  const [load, setLoad] = useState(false);
   return (
-    <div>
-      <h1>App Component</h1>
-      <button onClick={() => setLoad(true)}>Load</button>
-      {/* {load ? <ClassComponent /> : null} */}
-      {load ? (
-        <Suspense fallback={<h3>loading...</h3>}>
-          {" "}
-          <ClassComponent />{" "}
-        </Suspense>
-      ) : null}
-    </div>
+    <TodoList/>
   );
 }
 export default App;
